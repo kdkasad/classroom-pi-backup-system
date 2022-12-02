c1843c36ca482fde5db5d36e490e303f970115db
Kian Kasad <kian@kasad.com>
2022-12-01 17:19:03 -0800

    Add GPLv3 license

--- provision.sh
+++ provision.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+
+#
+#    Raspberry Pi Backup System for Classrooms - Node Provisioning Script
+#    Copyright (C) 2022  Kian Kasad
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License, version 3, as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
 # Change the following two lines to match your backup server
 SERVER_IP='10.205.8.217'
 SERVER_PORT=36888
