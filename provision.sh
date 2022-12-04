d62ad76c08a1da0c8dcd04a452a1a683c229bbe2
Kian Kasad <kian@kasad.com>
2022-12-03 20:55:25 -0800

    provision.sh: More verbosity!
    Prints an info message before each step to ease troubleshooting and
    pinpoint where errors occurred.
    
    Also makes it clear that the script is still working while not producing
    output, e.g. during apt-get(1) operations.

--- provision.sh
+++ provision.sh
@@ -27,37 +27,57 @@ SERVER_PORT=36888
 
 HOSTNAME_PATTERN='[A-Z0-9][A-Z0-9\-]*'
 
+info() {
+	printf '\033[1;34mInfo:\033[m ' >&2
+	printf "$@" >&2
+	printf '\n' >&2
+}
+
+warn() {
+	printf '\033[1;33mWarning:\033[m ' >&2
+	printf "$@" >&2
+	printf '\n' >&2
+}
+
+err() {
+	printf '\033[1;31mError:\033[m ' >&2
+	printf "$@" >&2
+	printf '\n' >&2
+}
+
 # Set new hostname given by user
 change_hostname() {
 	local response
 
 	# Prompt user for new hostname
-	printf 'Enter a new hostname: ' >&2
-	# Read from /dev/tty as stdin might be a pipe
+	# Read from/write to /dev/tty as stdin might be a pipe
+	printf 'Enter a new hostname: ' > /dev/tty
 	IFS='' read -r response < /dev/tty
 	until
 		printf '%s' "$response" | grep -ixq "$HOSTNAME_PATTERN"
 	do
-		printf '\033[1;31mError:\033[m %s %s\n' 'Invalid hostname.' \
-			'The hostname may only contain letters, numbers, and hyphens.'
-		printf 'Ente a new hostname: ' >&2
+		err 'The hostname may only contain letters, numbers, and hyphens.'
+		printf 'Enter a new hostname: ' > /dev/tty
 		IFS='' read -r response < /dev/tty
 	done
 
+	info "Setting new hostname to '%s'." "$response"
+	# Update /etc/hosts
+	sed -i "/^127\.0\.[01]\.1\s\+$(hostname)\s*$/d" /etc/hosts
+	printf '127.0.1.1\t%s\n' "$response" >> /etc/hosts
 	# Set new hostname. Attempts to use hostnamectl(1) first. If that fails,
 	# writes the new name to /etc/hostname then updates the live hostname using
 	# hostname(1) or /proc/sys/kernel/hostname.
-	printf "\033[1;34mInfo:\033[m Setting new hostname to '%s'.\n" "$response" >&2
-	sed -i "/^127\.0\.[01]\.1\s\+$(hostname)\s*$/d" /etc/hosts
-	printf '127.0.1.1\t%s\n' "$response" >> /etc/hosts
 	if ! \
 		hostnamectl --no-ask-password --static --pretty hostname "$response" \
 			2>/dev/null
 	then
+		warn 'Calling hostnamectl(1) failed. Falling back to hostname(1).'
 		printf '%s\n' "$response" > /etc/hostname
 		if ! \
 			hostname "$response" 2>/dev/null
 		then
+			warn 'Calling hostname(1) failed. Falling back to /proc/sys/kernel/hostname.'
 			printf '%s' "$response" > /proc/sys/kernel/hostname
 		fi
 	fi
@@ -68,11 +88,10 @@ set -e
 # Ensure we're running as root
 if [ $(id -u) -ne 0 ]
 then
-	printf '\033[1;31m%s\033[m %s\n' 'Error:' \
-		'This script must be run as root.' >&2
+	err 'This script must be run as root.'
 	if [ -x "$0" ]
 	then
-		printf "\033[1;32mHint:\033[m Try running 'sudo %s'.\n" "$0"
+		printf "\033[1;32mHint:\033[m Try running 'sudo %s'.\n" "$0" >&2
 	fi
 	exit 1
 fi
@@ -82,19 +101,24 @@ fi
 hostname="$(hostname)"
 if [ "$hostname" = "raspberrypi" ]
 then
-	printf "\033[1;33mWarning:\033[m You're using the default hostname '%s'.\n" "$hostname" >&2
+	warn "You're using the default hostname '%s'." "$hostname"
 	change_hostname
 elif ! printf '%s' "$hostname" | grep -ixq "$HOSTNAME_PATTERN"
 then
-	printf "\033[1;33mWarning:\033[m Invalid or no hostname detected.\n" >&2
+	warn 'Invalid or blank hostname detected.'
 	change_hostname
 fi
 
 # Install required packages
-apt-get update -qq
-apt-get install --no-install-recommends -qq \
+info 'Downloading package information...'
+info 'This may take a while.'
+apt-get -qq update
+info 'Installing required packages...'
+info 'This may take a while.'
+apt-get -qq install --no-install-recommends \
 	borgbackup openssh-client python3 python3-requests curl bash
 
+info 'Creating systemd units...'
 # Create systemd parent directories
 install -d -m 755 \
 	/usr/local/lib/systemd/system \
@@ -108,7 +132,6 @@ cat <<- 'EOF' > /usr/local/lib/systemd/system/backup.service
 Description=Back up user files
 Requires=network-online.target network.target
 After=network-online.target network.target
-Before=shutdown.target
 
 [Service]
 StateDirectory=backup_client
@@ -117,6 +140,8 @@ ExecStart=/usr/local/lib/backup_client.py
 EOF
 
 cat <<- 'EOF' > /etc/systemd/system/backup.service.d/00-triggers.conf
+[Unit]
+Before=shutdown.target
 [Install]
 WantedBy=shutdown.target
 EOF
@@ -138,12 +163,14 @@ EOF
 echo '[Timer]' > /etc/systemd/system/backup.timer.d/00-times.conf
 
 # Download SSH key
+info 'Downloading & installing SSH key...'
 umask 022
 install -d -m 755 /usr/local/share/backup_client
 curl -fsSL "http://$SERVER_IP:$SERVER_PORT/setup/node_key" \
 	> /usr/local/share/backup_client/ssh_key
 
 # Download client script
+info 'Downloading & installing client script...'
 install -d -m 755 /usr/local/lib
 umask 027
 curl -fsSL "http://$SERVER_IP:$SERVER_PORT/setup/client.py" \
@@ -152,8 +179,16 @@ chmod 750 /usr/local/lib/backup_client.py
 umask 022
 
 # Reload systemd and enable units
+info 'Reloading systemd configuration...'
 systemctl daemon-reload
-systemctl enable --now backup.service
-systemctl enable --now backup.timer
+info 'Enabling systemd units...'
+systemctl enable backup.service backup.timer
+info 'Running backup service...'
+systemctl start backup.service
+info 'Starting backup timer...'
+systemctl start backup.timer
+
+printf '\n'
+info 'Done!'
 
 exit 0
