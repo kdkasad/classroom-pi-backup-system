f362894da434e9a678d2ab057a6cfa30252ce2f9
Kian Kasad <kian@kasad.com>
2022-12-18 14:04:16 -0800

    find_missing.py: Add license notice

--- find_missing.py
+++ find_missing.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+
+#
+#    Raspberry Pi Backup System for Classrooms - Find missing backups script
+#    Copyright (C) 2022  Kian Kasad
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License, version 3, as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
 import argparse
 import asyncio
 from asyncio.subprocess import PIPE
